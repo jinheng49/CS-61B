fbe42eb577d3667918a002a720d7da904c6c70a5
jinheng49 <3172951601@qq.com>
2023-08-27 12:33:07 +0800

    completed firest part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,16 @@
+package ex;
+public class HelloNumbers {
+    public static void main(String[] args){
+        int x=0,sum=0;
+        while(x<9){
+             x=x+1;
+            sum=sum+x;
+           System.out.println(sum);
+        }
+        String h = 5 + "10";
+        System.out.println(h);
+
+
+    }
+    
+}
